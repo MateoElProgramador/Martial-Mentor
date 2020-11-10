13b3c3bb807624670ea1a867d8c2b40af2c0ec22
MateoElProgramador <mharding2@sheffield.ac.uk>
2020-11-10 23:01:01 +0000

    Add Game and Character models, register them to admin

--- mentor/models.py
+++ mentor/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=50)
+    short_title = models.CharField(max_length=20, blank=True)
+
+    """"""
+    def __str__(self):
+        return self.short_title if self.short_title != '' else self.title
+
+    def img_url(self):
+        return 'images/games/' + self.id + '/game_cover.png'
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=50)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def img_url(self):
+        return 'images/games/' + self.game.id + '/characters/' + self.name + '.png'

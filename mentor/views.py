c42a6bd1d9bf47e6d94d20749e5a907eac1a0e9d
MateoElProgramador <mharding2@sheffield.ac.uk>
2020-11-11 18:15:40 +0000

    Comment out unused character image filter

--- mentor/views.py
+++ mentor/views.py
@@ -4,23 +4,23 @@ from django.template.defaultfilters import register
 from django.templatetags.static import static
 from django.views import generic
 
-from martialmentor.settings import STATIC_ROOT
+# from martialmentor.settings import STATIC_ROOT
 from mentor.models import Game
 
 
 # TODO: Figure out how to handle static files properly
 # --- Not currently used: ---
 @register.filter(name='file_exists')
-def file_exists(filepath):
-    print(STATIC_ROOT + '/static/' + filepath)
-    if default_storage.exists(STATIC_ROOT + '/static/' + filepath):
-        print("img found!")
-        return static(filepath)
-    else:
-        print("img not found at", filepath)
-        index = filepath.rfind('/')
-        new_filepath = '/mentor/images/char_placeholder.jpg'
-        return new_filepath
+# def file_exists(filepath):
+#     print(STATIC_ROOT + '/static/' + filepath)
+#     if default_storage.exists(STATIC_ROOT + '/static/' + filepath):
+#         print("img found!")
+#         return static(filepath)
+#     else:
+#         print("img not found at", filepath)
+#         index = filepath.rfind('/')
+#         new_filepath = '/mentor/images/char_placeholder.jpg'
+#         return new_filepath
 
 
 class GameIndexView(generic.ListView):

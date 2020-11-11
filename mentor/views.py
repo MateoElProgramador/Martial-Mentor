7fec0575cb7acc111f85a39d20cafec70129c7b7
MateoElProgramador <mharding2@sheffield.ac.uk>
2020-11-11 04:32:39 +0000

    Add broken filter for char img check; struggled with static file handling

--- mentor/views.py
+++ mentor/views.py
@@ -1,6 +1,26 @@
+from django.core.files.storage import default_storage
 from django.shortcuts import render
+from django.template.defaultfilters import register
+from django.templatetags.static import static
 from django.views import generic
+
+from martialmentor.settings import STATIC_ROOT
 from mentor.models import Game
+
+
+# TODO: Figure out how to handle static files properly
+# --- Not currently used: ---
+@register.filter(name='file_exists')
+def file_exists(filepath):
+    print(STATIC_ROOT + '/static/' + filepath)
+    if default_storage.exists(STATIC_ROOT + '/static/' + filepath):
+        print("img found!")
+        return static(filepath)
+    else:
+        print("img not found at", filepath)
+        index = filepath.rfind('/')
+        new_filepath = '/mentor/images/char_placeholder.jpg'
+        return new_filepath
 
 
 class GameIndexView(generic.ListView):

dc125c72795c0bf23335945d86cbde92a6a8d2d7
MateoElProgramador <mharding2@sheffield.ac.uk>
2020-11-13 02:32:54 +0000

    Refactor to template inheritance structure, add navbar

--- martialmentor/settings.py
+++ martialmentor/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'martialmentor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'mentor/templates/mentor')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

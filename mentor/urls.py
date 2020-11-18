726aa0190552afc35c79cdb9083e05bed48fc98c
MateoElProgramador <mharding2@sheffield.ac.uk>
2020-11-18 03:25:11 +0000

    Add no characters message on character overlay page, add trailing / to es-toggle path

--- mentor/urls.py
+++ mentor/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('game/', views.GameIndexView.as_view(), name='alt_index'),
     path('game/<int:game_id>/', views.tools, name='tools'),
     path('game/<int:game_id>/char-overlay/', character_overlay, name='char_overlay'),
-    path('game/<int:game_id>/es-toggle', elite_smash_toggle, name='elite_smash_toggle'),
+    path('game/<int:game_id>/es-toggle/', elite_smash_toggle, name='elite_smash_toggle'),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]

d5c9340799777d54a38f44737927f901019e0e38
MateoElProgramador <mharding2@sheffield.ac.uk>
2020-12-08 02:25:28 +0000

    Move CSRF validation for Ajax requests into own js file

--- mentor/static/mentor/scripts/char_overlay.js
+++ mentor/static/mentor/scripts/char_overlay.js
@@ -1,19 +1,3 @@
-// Get csrf token from DOM:
-var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
-
-// --- Ensure csrf token is sent along with any Ajax request: ---
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    }
-});
-
 // Toggle background colour of body upon changing of chroma key checkbox:
 $('#chroma_checkbox').change(function() {
       $('body').toggleClass('chromakey');
